a989032e56e93a8224617d8eaa3ddb5928315635
Rodolfo Mendes <rodolfo.m.s.mendes@gmail.com>
2026-09-06 16:08:58 -0300

    - Refactoring User to AppUser to avoid naming conflicts

--- mychat-backend/src/main/java/mychat/domain/AppUser.java
+++ mychat-backend/src/main/java/mychat/domain/AppUser.java
@@ -4,8 +4,7 @@ import jakarta.validation.constraints.NotBlank;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.relational.core.mapping.Table;
 
-@Table("app_user")
-public record User(
+public record AppUser(
     @Id Long id,
     @NotBlank String email,
     @NotBlank String passwordHash
--- mychat-backend/src/main/java/mychat/domain/Chat.java
+++ mychat-backend/src/main/java/mychat/domain/Chat.java
@@ -4,16 +4,14 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.jdbc.core.mapping.AggregateReference;
-import org.springframework.data.relational.core.mapping.Column;
 
 import java.time.OffsetDateTime;
 
 public record Chat(
     @Id
     Long id,
-    @Column("app_user_id")
     @NotNull
-    AggregateReference<User, Long> userId,
+    AggregateReference<AppUser, Long> appUserId,
     @NotBlank
     String title,
     OffsetDateTime createdAt
